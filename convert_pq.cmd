8a83c67cc77861d98bc431b5d3f0f5abbbf9fc21
Alexander Tretyak <alextretyak@users.noreply.github.com>
2021-03-03 09:47:40 +1000

    .

---
+++ convert_pq.cmd
@@ -0,0 +1,4 @@
+python C:\!!BITBUCKET\pqmarkup\pqmarkup.py --output-html-document .pq -f index.html
+@if not %ERRORLEVEL% == 0 pause
+python C:\!!BITBUCKET\pqmarkup\pqmarkup.py --output-html-document ru.pq -f ru\index.html
+@if not %ERRORLEVEL% == 0 pause
